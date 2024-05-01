de83e369c564891259950f9e86edfe86f1edef3b
이영우 <zestlee1106@gmail.com>
2024-05-01 15:01:36 +0900

    first init

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/navigation";
+
+export default function NotFound() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Not found!</h1>
+    </div>
+  );
+}
---
+++ components/navigation.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+  const path = usePathname();
+  console.log(path);
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link> {path === "/" ? "🐼" : ""}
+        </li>
+        <li>
+          <Link href="/about-us">About Us</Link>{" "}
+          {path === "/about-us" ? "🐼" : ""}
+        </li>
+      </ul>
+    </nav>
+  );
+}
