32d70ebbb6d1d711afbdf2e39ec6b25e59ae5e1c
이영우 <zestlee1106@gmail.com>
2024-05-04 14:53:39 +0900

    💄 movie info 데이터 연동 추가 및 스타일 추가

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
 async function getMovie(id: string) {
   return await fetch(`${API_URL}/${id}`).then((res) => res.json());
@@ -6,5 +7,21 @@ async function getMovie(id: string) {
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img
+        src={movie.poster_path}
+        alt={movie.title}
+        className={styles.poster}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target="_blank">
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
