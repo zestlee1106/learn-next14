bbaa57e1dd5d7dbab3a9612499e79ee42c0172ee
이영우 <zestlee1106@gmail.com>
2024-05-04 13:43:42 +0900

    🎨 navigation 스타일 추가

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,12 +2,13 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
-  console.log(path);
+
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href="/">Home</Link> {path === "/" ? "🐼" : ""}
